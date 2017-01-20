1f3f8e8647e87fe717f13abe008b03ced9026b58
Maxim Pavlov <maxpav137@gmail.com>
2017-01-21 00:25:22 +0500

    Форматирование+имена функций

--- RTH/views/main_window.py
+++ RTH/views/main_window.py
@@ -1,6 +1,6 @@
 import os
 from PyQt5.QtCore import QSettings
-from PyQt5.QtWidgets import QMainWindow, QFileSystemModel, QFileDialog, QMessageBox, QTreeView
+from PyQt5.QtWidgets import QMainWindow, QFileSystemModel, QFileDialog, QMessageBox
 
 from RTH.handlers import xml_handler
 from RTH.ui.main import Ui_MainWindow
@@ -20,10 +20,10 @@ class MainUI(QMainWindow, Ui_MainWindow):
         self.show()
 
     def connect_signals(self):
-        self.save.triggered.connect(self.save_xml)
-        self.tab_widget.tabCloseRequested.connect(self.close_xml)
+        self.save.triggered.connect(self.save_file)
+        self.tab_widget.tabCloseRequested.connect(self.close_file)
         self.open_folder.triggered.connect(self.open_project)
-        self.project_view.doubleClicked.connect(lambda m: self.open_xml(self.project_view.model().filePath(m)))
+        self.project_view.doubleClicked.connect(lambda m: self.open_file(self.project_view.model().filePath(m)))
 
     def load_settings(self):
         pass
@@ -34,7 +34,8 @@ class MainUI(QMainWindow, Ui_MainWindow):
     def open_project(self):
         dir_path = QFileDialog.getExistingDirectory(self, options=QFileDialog.ShowDirsOnly)
         if not os.path.exists(os.path.join(dir_path, 'About', 'About.xml')):
-            QMessageBox.warning(self, 'RimworldTranslationHelper', 'Укажите директорию, которая содержит мод для игры Rimworld', buttons=QMessageBox.Ok)
+            QMessageBox.warning(self, 'RimworldTranslationHelper',
+                                'Укажите директорию, которая содержит мод для игры Rimworld', buttons=QMessageBox.Ok)
             return
         project_model = QFileSystemModel()
         project_model.setRootPath(dir_path)
@@ -43,7 +44,7 @@ class MainUI(QMainWindow, Ui_MainWindow):
         for col_number in range(1, 4):
             self.project_view.setColumnHidden(col_number, True)
 
-    def open_xml(self, file_path):
+    def open_file(self, file_path):
         if os.path.isdir(file_path):  # TODO
             return
         if file_path in self.opened_files:
@@ -59,20 +60,23 @@ class MainUI(QMainWindow, Ui_MainWindow):
         try:
             view = handler(file_path)
         except Exception as error_msg:
-            QMessageBox.critical(self, 'RimworldTranslationHelper', 'При открытии файла произошла ошибка:\n{}'.format(error_msg), buttons=QMessageBox.Ok)
+            QMessageBox.critical(self, 'RimworldTranslationHelper',
+                                 'При открытии файла произошла ошибка:\n{}'.format(error_msg), buttons=QMessageBox.Ok)
             return
         index = self.tab_widget.addTab(view, os.path.basename(file_path))
         self.tab_widget.setCurrentIndex(index)
         self.opened_files[file_path] = {'view': view}
 
-    def close_xml(self, index):
+    def close_file(self, index):
         widget = self.tab_widget.widget(index)
         file_path = widget.model().file_path
         self.opened_files.pop(file_path)
         self.tab_widget.removeTab(index)
         widget.deleteLater()  # TODO Make some tests
 
-    def save_xml(self):
+    def save_file(self):
+        if not self.opened_files:
+            return
         widget = self.tab_widget.currentWidget()
         model = widget.model()
         if not model.is_modified:
